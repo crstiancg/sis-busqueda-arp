3fbcddf7c608d7e79627dd3559edd3ce15bdcd71
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-12-11 12:46:53 -0500

    NOTARIO Y SUBSERIE

--- front-sisbusqueda/src/layouts/AdminLayout.vue
+++ front-sisbusqueda/src/layouts/AdminLayout.vue
@@ -86,6 +86,36 @@
             <q-item-section> Usuarios </q-item-section>
           </q-item>
 
+          <q-item
+            :to="{ name: 'Notarios' }"
+            :active="link === 'Notarios'"
+            @click="link = 'Notarios'"
+            clickable
+            v-ripple
+            class="q-ma-xs rounded-borders"
+            active-class="my-menu-link"
+          >
+            <q-item-section avatar>
+              <q-icon name="mdi-account" />
+            </q-item-section>
+
+            <q-item-section> Notarios </q-item-section>
+          </q-item>
+          <q-item
+            :to="{ name: 'SubSeries' }"
+            :active="link === 'SubSeries'"
+            @click="link = 'SubSeries'"
+            clickable
+            v-ripple
+            class="q-ma-xs rounded-borders"
+            active-class="my-menu-link"
+          >
+            <q-item-section avatar>
+              <q-icon name="mdi-account" />
+            </q-item-section>
+
+            <q-item-section> SubSeries </q-item-section>
+          </q-item>
           <q-separator />
     <!-- ruta de prueba  **************************************************** -->
           <q-item
